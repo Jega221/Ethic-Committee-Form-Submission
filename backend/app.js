7de5e10fa6f9cbdfe4c23c791f170d8d024541d9
KingAbeni <graceabenimoi@gmail.com>
2025-10-28 00:53:40 +0200

    db connection, authentication with bcrypt and jwt

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,33 @@
 // index.js
 const express = require('express');
 const cors = require('cors');
+const authRouter = require('./routes/auth');
+const authMiddleware = require('./middlewares/authMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+/*CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100) UNIQUE NOT NULL
+);
+POST http://localhost:5000/api/auth/signup
+Body (JSON):
+
+{
+  "name": "Alice",
+  "email": "alice@example.com",
+  "password": "secret123"
+}
+POST http://localhost:5000/api/auth/login
+Body:
+
+{
+  "email": "alice@example.com",
+  "password": "secret123"
+}
+*/
 
 
 app.use(cors());           // Enable CORS
@@ -12,6 +36,11 @@ app.use(express.json());   // Parse JSON bodies
 app.get('/', (req, res) => {
   res.send('Express server is running');
 });
+// Auth routes
+app.use('/api/auth', authRouter);
+
+// Protected routes
+app.use('/api/users', authMiddleware, usersRouter);
 
 // Start server
 app.listen(PORT, () => {
