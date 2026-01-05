cf30e96155b451c00d6282145d44a01710e0592f
KingAbeni <graceabenimoi@gmail.com>
2026-01-05 16:17:25 +0200

    backend starting fix

--- backend/create_super_test.js
+++ backend/create_super_test.js
@@ -1,5 +1,5 @@
 const pool = require('./db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 async function createSuperAdmin() {
     const email = 'antigravity_super@test.com';
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,7 @@
 // backend/routes/admin.js
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
 const { verifyToken, isSuperAdmin } = require('../middlewares/superAdminMiddelware');
